a6348a2860b91c9df212125a3bdf20404d6dbe74
DharshanRajPA <dharshanrajarunkumar@gmail.com>
2025-08-03 02:39:22 +0530

    prisma added

--- payment-webapp/apps/user-webapp/app/page.tsx
+++ payment-webapp/apps/user-webapp/app/page.tsx
@@ -1,12 +1,15 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { PrismaClient } from "@repo/db/client";
 
+const prisma = new PrismaClient();
 
 export default function Home() {
   return (
     <div className="text-3xl font-bold underline">
       Hello World
+      <br />
       <Button appName="User Webapp" className="bg-blue-500 text-white p-2 rounded-md">Click me</Button>
     </div>
   );
