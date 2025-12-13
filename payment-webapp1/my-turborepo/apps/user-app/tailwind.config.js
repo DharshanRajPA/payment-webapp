5c3157a0a52726faf9695eff1c826aa746c22b0b
DharshanRajPA <dharshanrajarunkumar@gmail.com>
2025-12-13 17:58:31 +0530

    add monorepos

---
+++ payment-webapp1/my-turborepo/apps/user-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#6366f1",
+      },
+    },
+  },
+  plugins: [],
+}
